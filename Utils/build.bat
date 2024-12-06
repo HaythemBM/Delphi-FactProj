d942b99ff2830b3e12976094027e798fd3984708
HBMessouad <haythem.benmessaoud@ensi-uma.tn>
2024-12-06 10:18:55 +0100

    add run batch file and refactoring project

--- Utils/build.bat
+++ Utils/build.bat
@@ -1,3 +1,5 @@
+rem calling batch file to set up env variables
 call "C:\Program Files (x86)\Embarcadero\RAD Studio\8.0\bin\rsvars.bat"
+rem building project
 cd /D %1
 msbuild
---
+++ Utils/run.bat
@@ -0,0 +1,4 @@
+rem accessing binary directory
+cd .\Debug\Win32
+rem running project
+FactProj.exe
